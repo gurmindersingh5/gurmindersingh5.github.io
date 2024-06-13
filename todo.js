36adefa53651fb39c94eeddeb30ee7b876f667fd
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2024-06-13 00:18:55 -0400

    Delete todo.js

--- todo.js
+++
@@ -1,9 +0,0 @@
-function addTask() {
-    const task = document.getElementById('new-task').value;
-    if (task) {
-        const li = document.createElement('li');
-        li.innerText = task;
-        document.getElementById('task-list').appendChild(li);
-        document.getElementById('new-task').value = '';
-    }
-}
