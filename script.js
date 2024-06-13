0f2c1982caf1e3b0c558bfd3ac9e17700d8885ff
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2024-06-13 01:00:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ document.addEventListener("DOMContentLoaded", function() {
 function startLoading() {
     const animationContainer = document.getElementById('animation-container');
     const loader = document.getElementById('loader');
-    const content = document.getElementById('content');
 
     animationContainer.style.display = 'none';
     loader.style.display = 'block';
 
     setTimeout(function() {
         loader.style.display = 'none';
-        content.style.display = 'block';
+        // Replace 'your-github-repo-url' with the actual URL of your GitHub repo README.md
+        window.location.href = 'https://github.com/gurmindersingh5/gurmindersingh5.github.io/blob/main/README.md';
     }, 1000); // 1000ms = 1 second
 }
