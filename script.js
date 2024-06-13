f342b579e810cae4af3fc797407186c4442c7509
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2024-06-13 00:56:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Animation complete event
-    const animationContainer = document.getElementById('animation-container');
     const mouse = document.getElementById('mouse');
     const viewResumeBtn = document.getElementById('view-resume-btn');
 
     mouse.addEventListener('animationend', function() {
         viewResumeBtn.click();
     });
-
-    viewResumeBtn.addEventListener('click', function() {
-        animationContainer.style.display = 'none';
-        document.getElementById('loader').style.display = 'block';
-        
-        setTimeout(function() {
-            document.getElementById('loader').style.display = 'none';
-            document.getElementById('content').style.display = 'block';
-        }, 1000); // 1000ms = 1 second
-    });
 });
+
+function startLoading() {
+    const animationContainer = document.getElementById('animation-container');
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+
+    animationContainer.style.display = 'none';
+    loader.style.display = 'block';
+
+    setTimeout(function() {
+        loader.style.display = 'none';
+        content.style.display = 'block';
+    }, 1000); // 1000ms = 1 second
+}
