106420151e743d3b313fa89fc261584ef5dd27a8
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2024-06-13 00:29:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.getElementById('loader').style.display = 'none';
-        document.getElementById('content').style.display = 'block';
+        document.getElementById('content').style.display = 'flex';
     }, 3000); // 3000ms = 3 seconds
 });
