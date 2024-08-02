2d6afd842979545f6e2882f96b7fda8b250e9e78
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2024-08-01 23:33:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
-    window.location.href = 'https://gurmindersingh5.github.io/Gurminder_resume/';
+    window.location.href = 'https://gurmindersingh5.github.io/portfolio/';
 });
